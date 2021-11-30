0663a5aededf67666b33eeac37f2c906fd7c9b4d
F. Ahmed <19439535+hicka@users.noreply.github.com>
2021-11-30 14:53:30 +0500

    added first working test

---
+++ tests/Feature/ExampleTest.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Seyls\Accounting\Tests\Feature;
+
+use Seyls\Accounting\Tests\TestCase;
+
+class ExampleTest extends TestCase
+{
+    public function testExampleTest()
+    {
+        $this->assertTrue(true);
+    }
+}
